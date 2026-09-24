2cea85268f3b5bd7f7cd948b6fa2573339e525a0
saiparthiv0deshpande <153856335+saiparthiv0deshpande@users.noreply.github.com>
2026-09-25 02:02:07 +0530

    Store encoded bit count in file header

--- include/FileFormat.h
+++ include/FileFormat.h
@@ -8,14 +8,14 @@
 
 namespace HuffFile {
 
-constexpr uint8_t VERSION = 1;
+constexpr uint8_t VERSION = 2;
 constexpr uint8_t MAGIC[4] = {'H', 'U', 'F', 'F'};
 
 struct Header {
     uint64_t originalSize = 0;
+    uint64_t encodedBits = 0;
     std::array<uint64_t, 256> frequencies{};
     uint16_t uniqueSymbols = 0;
-    uint8_t paddingBits = 0;
 };
 
 void writeHeader(std::ostream& output, const Header& header);
