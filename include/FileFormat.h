8fd95d0ff6644d1a6857d2e274e7894ce9f34924
saiparthiv0deshpande <153856335+saiparthiv0deshpande@users.noreply.github.com>
2026-09-25 01:54:47 +0530

    Implement Huffman file format and compressor interfaces

--- include/FileFormat.h
+++ include/FileFormat.h
@@ -1,6 +1,26 @@
 #ifndef FILE_FORMAT_H
 #define FILE_FORMAT_H
 
-// Custom .huff file format definitions will be added here.
+#include <array>
+#include <cstdint>
+#include <istream>
+#include <ostream>
+
+namespace HuffFile {
+
+constexpr uint8_t VERSION = 1;
+constexpr uint8_t MAGIC[4] = {'H', 'U', 'F', 'F'};
+
+struct Header {
+    uint64_t originalSize = 0;
+    std::array<uint64_t, 256> frequencies{};
+    uint16_t uniqueSymbols = 0;
+    uint8_t paddingBits = 0;
+};
+
+void writeHeader(std::ostream& output, const Header& header);
+Header readHeader(std::istream& input);
+
+} // namespace HuffFile
 
 #endif
