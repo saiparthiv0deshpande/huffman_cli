88244e72e313ec8e455d806ea28a8292ec066cbf
saiparthiv0deshpande <153856335+saiparthiv0deshpande@users.noreply.github.com>
2026-09-25 02:03:04 +0530

    Add compressed file information API

--- include/Compressor.h
+++ include/Compressor.h
@@ -10,6 +10,14 @@ struct CompressionStats {
     double compressionRatio = 0.0;
 };
 
+struct FileInfo {
+    uint64_t originalSize = 0;
+    uint64_t compressedSize = 0;
+    uint64_t encodedBits = 0;
+    uint16_t uniqueSymbols = 0;
+    double compressionRatio = 0.0;
+};
+
 CompressionStats compressFile(
     const std::string& inputPath,
     const std::string& outputPath);
@@ -17,5 +25,7 @@ CompressionStats compressFile(
 CompressionStats decompressFile(
     const std::string& inputPath,
     const std::string& outputPath);
+
+FileInfo getFileInfo(const std::string& inputPath);
 
 #endif
