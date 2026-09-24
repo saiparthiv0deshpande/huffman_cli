05012d18214914420a4c0723228c0f04720980a6
saiparthiv0deshpande <153856335+saiparthiv0deshpande@users.noreply.github.com>
2026-09-25 01:54:21 +0530

    Implement bit-level I/O: include/BitReader.h

--- include/BitReader.h
+++ include/BitReader.h
@@ -1,9 +1,21 @@
 #ifndef BIT_READER_H
 #define BIT_READER_H
 
+#include <cstdint>
+#include <istream>
+
 class BitReader {
+private:
+    std::istream& input;
+    uint8_t buffer = 0;
+    uint8_t bitsRemaining = 0;
+    uint64_t totalBits = 0;
+
 public:
-    // Bit-level input implementation coming next.
+    explicit BitReader(std::istream& input);
+
+    bool readBit(bool& bit);
+    uint64_t bitsRead() const;
 };
 
 #endif
